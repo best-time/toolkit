65349d455dc8a22899708e1484ae791c1141ae85
yinweiyi <349057996@qq.com>
2015-10-18 00:40:40 +0800

    js

--- js/hd.js
+++ js/hd.js
@@ -1,3 +1,170 @@
-!function() {
-	var root = this;
+! function() {
+    // "use strict";
+    var root = this;
+    var previousHD = root._hd;
+
+    var ArrayProto = Array.prototype,
+        ObjProto = Object.prototype;
+
+    //快速获取原型上的方法
+    var
+        push = ArrayProto.push,
+        slice = ArrayProto.slice,
+        toString = ObjProto.toString,
+        hasOwnProperty = ObjProto.hasOwnProperty;
+
+    var _hd = function(obj) {
+        if (obj instanceof _hd) return obj;
+        if (!(this instanceof _hd)) return new _hd(obj);
+        this._wrapped = obj;
+    };
+
+    if (typeof exports != 'undefined' && !exports.nodeType) {
+        if (typeof module != 'undefined' && !module.nodeType && module.exports) {
+            exports = module.exports = _hd;
+        }
+        exports._hd = _hd;
+    } else {
+        root._hd = _hd;
+    }
+
+    // Internal function that returns an efficient (for current engines) version
+    // of the passed-in callback, to be repeatedly applied in other Underscore
+    // functions.
+    var optimizeCb = function(func, context, argCount) {
+        if (context === void 0) return func;
+        switch (argCount == null ? 3 : argCount) {
+            case 1:
+                return function(value) {
+                    return func.call(context, value);
+                };
+                // The 2-parameter case has been omitted only because no current consumers
+                // made use of it.
+            case 3:
+                return function(value, index, collection) {
+                    return func.call(context, value, index, collection);
+                };
+            case 4:
+                return function(accumulator, value, index, collection) {
+                    return func.call(context, accumulator, value, index, collection);
+                };
+        }
+        return function() {
+            return func.apply(context, arguments);
+        };
+    };
+
+    // A mostly-internal function to generate callbacks that can be applied
+    // to each element in a collection, returning the desired result — either
+    // `identity`, an arbitrary callback, a property matcher, or a property accessor.
+    var cb = function(value, context, argCount) {
+        if (value == null) return _.identity;
+        if (_.isFunction(value)) return optimizeCb(value, context, argCount);
+        if (_.isObject(value)) return _.matcher(value);
+        return _.property(value);
+    };
+
+    // An internal function for creating a new object that inherits from another.
+    var baseCreate = function(prototype) {
+        if (!_.isObject(prototype)) return {};
+        if (nativeCreate) return nativeCreate(prototype);
+        Ctor.prototype = prototype;
+        var result = new Ctor;
+        Ctor.prototype = null;
+        return result;
+    };
+
+    var property = function(key) {
+        return function(obj) {
+            return obj == null ? void 0 : obj[key];
+        };
+    };
+
+    // Helper for collection methods to determine whether a collection
+    // should be iterated as an array or as an object.
+    // Related: http://people.mozilla.org/~jorendorff/es6-draft.html#sec-tolength
+    // Avoids a very nasty iOS 8 JIT bug on ARM-64. #2094
+    var MAX_ARRAY_INDEX = Math.pow(2, 53) - 1;
+    var getLength = property('length');
+    var isArrayLike = function(collection) {
+        var length = getLength(collection);
+        return typeof length == 'number' && length >= 0 && length <= MAX_ARRAY_INDEX;
+    };
+
+
+    _hd.each = function(obj, iteratee, context) {
+        iteratee = optimizeCb(iteratee, context);
+        var i, length;
+        if (isArrayLike(obj)) {
+            for (i = 0, length = obj.length; i < length; i++) {
+                iteratee(obj[i], i, obj);
+            }
+        } else {
+            var keys = _.keys(obj);
+            for (i = 0, length = keys.length; i < length; i++) {
+                iteratee(obj[keys[i]], keys[i], obj);
+            }
+        }
+        return obj;
+    };
+
+    // Return the results of applying the iteratee to each element.
+    _hd.map = function(obj, iteratee, context) {
+        iteratee = cb(iteratee, context);
+        var keys = !isArrayLike(obj) && _.keys(obj),
+            length = (keys || obj).length,
+            results = Array(length);
+        for (var index = 0; index < length; index++) {
+            var currentKey = keys ? keys[index] : index;
+            results[index] = iteratee(obj[currentKey], currentKey, obj);
+        }
+        return results;
+    };
+
+    /*---------------------------------------
+    ===================================================
+    	工具
+    */
+
+    //返回当前时间的时间戳(毫秒)
+    _hd.now = Date.now || function() {
+        return new Date().getTime();
+    };
+
+    //返回min-max之间的一个随机整数, 包括min,不包括max值
+    _hd.random = function(min, max) {
+        if (max == null) {
+            max = min;
+            min = 0;
+        }
+        return min + Math.floor(Math.random() * (max - min + 1));
+    };
+
+    //判断对象上是否含有变量key这个属性
+    _hd.has = function(obj, key) {
+        return obj != null && hasOwnProperty.call(obj, key);
+    };
+
+    _hd.each(['Arguments', 'Function', 'String', 'Number', 'Date', 'RegExp', 'Error'], function(name) {
+        _hd['is' + name] = function(obj) {
+            return toString.call(obj) === '[object ' + name + ']';
+        };
+    });
+
+    _hd.isFinite = function(obj) {
+        return isFinite(obj) && !isNaN(parseFloat(obj));
+    };
+
+    _hd.isNaN = function(obj) {
+        return _.isNumber(obj) && isNaN(obj);
+    };
+
+    _hd.isBoolean = function(obj) {
+        return obj === true || obj === false || toString.call(obj) === '[object Boolean]';
+    };
+    //避免_hd变量的冲突
+    _hd.noConflict = function() {
+        root._hd = previousHD;
+        return this;
+    };
 }()
