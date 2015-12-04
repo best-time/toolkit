8485c32e266f79454e5a5d409e6ff1e183c4dbcd
349057996@qq.com <ywy>
2015-12-04 16:31:11 +0800

    hd.js

--- js/hd.js
+++ js/hd.js
@@ -363,9 +363,9 @@
 
     //阶乘
     _sole.factorial = function(num, parm) {
-        if(!_sole.isNumber(num)) return;
+        if (!_sole.isNumber(num)) return;
         var res = parm || 1;
-        if(num < 2) {
+        if (num < 2) {
             return res;
         }
         return factorial(num - 1, res * num); //arguments.callee
@@ -534,4 +534,18 @@
         root._sole = previousHD;
         return this;
     };
+    //返回ie版本号 6/7/8/9
+    var ie = (function() {
+        var undef,
+            v = 3,
+            div = document.createElement('div'),
+            all = div.getElementsByTagName('i');
+
+        while (
+            div.innerHTML = '<!--[if gt IE ' + (++v) + ']><i></i><![endif]-->',
+            all[0]
+        );
+
+        return v > 4 ? v : undef;
+    }());
 }();
