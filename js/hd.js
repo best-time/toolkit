fa031867adfbe11302f5c6e24e5e52b4393ea483
349057996@qq.com <ywy>
2015-12-07 17:40:48 +0800

    note of js-mode

--- js/hd.js
+++ js/hd.js
@@ -327,6 +327,30 @@
             }
         }
     };
+    /**
+     *    var dad = {
+            counter: [1, 2, 3],
+            reads: {paper: true}
+        }
+        var kkid = extendDeep(dad);
+     */
+    function extendDeep(parent, child) {
+        var i,
+            toStr = Object.prototype.toString,
+            astr = "[object Array]";
+        var child = child || {};
+        for (i in parent) {
+            if (parent.hasOwnProperty(i)) {
+                if (typeof parent[i] === "object") {
+                    child[i] = (toStr.call(parent[i]) === astr) ? [] : {};
+                    extendDeep(parent[i], child[i]);
+                } else {
+                    child[i] = parent[i];
+                }
+            }
+        }
+        return child;
+    }
 
     //设置cookie
     _sole.setCookie = function(name, value, hours) { //写cookie
