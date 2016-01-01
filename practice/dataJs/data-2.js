55ba9794945cb03b2ad5c2e6f459334ebb4a7ce2
sole <349057996@qq.com>
2016-01-01 10:49:09 +0800

    note of dataJs

--- practice/dataJs/data-2.js
+++ practice/dataJs/data-2.js
@@ -12,7 +12,7 @@
  * 但实例.__proto__上存在这些方法, 实例.__proto__指向构造函数Data的原型,
  * 也就相当于调用Data自身原型上的 get set has sub unsub 方法.
  */
-/*
+
 (function (root, factory) {
     //返回Data构造函数
     var Data = factory(root);
@@ -35,8 +35,7 @@
         };
         root.Data = Data;
     }
-}(this,*/
-    (function (root) {
+}(this, function (root) {
     'use strict';
     var slice = [].slice;
     var obj = {};
@@ -165,77 +164,137 @@
         };
     };
 
-    Data.prototype.set = function (key, val) {
-        var ctx = this._context;
-        if (typeof key !== 'string') return false;  //第一个参数不是字符串 ,直接返回false
+    //扩展Data原型
+    extendDeep(Data.prototype, {  //扩展Data原型上的方法
+        /*_init: function () {
+            //this._conotext = {};
+            //this._events = {
+            //    'set': {},
+            //    'delete': {},
+            //    'add': {},
+            //    'update': {}
+            //};
+        },*/
+        set: function (key, val) {
+            var ctx = this._context;
+            if (typeof key !== 'string') return false;  //第一个参数不是字符串 ,直接返回false
 
-        var keys = parseKey(key);
-        var len = keys.length;
-        var src;
+            var keys = parseKey(key);
+            var len = keys.length;
+            var src;
 
-        if (len < 2) {      //键值为 单个的情况
-            src = {};
-            src[key] = val;
-            extendData(undefined, this._events, ctx, src);
+            if (len < 2) {      //键值为 单个的情况
+                src = {};
+                src[key] = val;
+                extendData(undefined, this._events, ctx, src);
+                return true;
+            }
+            var name;
+            var i = 0;
+            //切换到对应上下文
+            for (; i < len - 1; i++) {
+                name = keys[i];
+                //若不存在对应上下文自动创建
+                if (!isArr(ctx[name]) && !isObj(ctx[name])) {
+                    //删除操作不存在对应值时，提前退出
+                    if (typeof val === 'undefined') {
+                        return false;
+                    }
+                    //若键值为数组则新建数组，否则新建对象
+                    ctx[name] = isNaN(Number(name)) ? {} : [];
+                }
+                ctx = ctx[name];
+            }
+
+            name = keys.pop();
+            src = isArr(ctx) ? [] : {};
+            src[name] = val;
+            ctx = extendData(keys.join('.'), this._events, ctx, src);
+
+            return true;
+        },
+        get: function (key) {
+            //key不为字符串返回undefined
+            if (typeof key !== 'string') return undefined;
+
+            var keys = parseKey(key);
+            var len = keys.length;
+            var i = 0;
+            var ctx = this._context;
+            var name;
+
+            for (; i < len; i++) {
+                name = keys[i];
+                ctx = ctx[name];
+
+                if (typeof ctx === 'undefined' || ctx === null) {
+                    return ctx;
+                }
+            }
+            //返回数据的副本
+            return cloneDeep(ctx);
+        },
+        has: function (key) {
+            return typeof  this.get(key) === 'undefined' ? false : true;
+        },
+        sub: function (type, key, callback) {
+            if (typeof type !== 'string' || typeof key !== 'string' || !isFun(callback)) {
+                return -1;
+            }
+
+            var events = this._events[type] || {};
+
+            events[key] = events[key] || {};
+
+            events[key][euid++] = callback;
+
+            return euid - 1;
+        },
+        unsub: function (type, key, id) {
+            if (typeof type !== 'string' || typeof key !== 'string') {
+                return false;
+            }
+
+            var events = this._events[type] || {};
+
+            if (!isObj(events[key])) return false;
+
+            if (typeof id !== 'number') {
+                delete events[key];
+                return true;
+            }
+
+            delete events[key][id];
+
             return true;
         }
-        var name;
-        var i = 0;
-        //切换到对应上下文
-        for (; i < len - 1; i++) {
-            name = keys[i];
-            //若不存在对应上下文自动创建
-            if (!isArr(ctx[name]) && !isObj(ctx[name])) {
-                //删除操作不存在对应值时，提前退出
-                if (typeof val === 'undefined') {
-                    return false;
-                }
-                //若键值为数组则新建数组，否则新建对象
-                ctx[name] = isNaN(Number(name)) ? {} : [];
-            }
-            ctx = ctx[name];
-        }
-
-        name = keys.pop();
-        src = isArr(ctx) ? [] : {};
-        src[name] = val;
-        ctx = extendData(keys.join('.'), this._events, ctx, src);
-
-        return true;
-    };
-    Data.prototype.get = function (key) {
-        //key不为字符串返回undefined
-        if (typeof key !== 'string') return undefined;
-
-        var keys = parseKey(key);
-        var len = keys.length;
-        var i = 0;
-        var ctx = this._context;
-        var name;
-
-        for (; i < len; i++) {
-            name = keys[i];
-            ctx = ctx[name];
-
-            if (typeof ctx === 'undefined' || ctx === null) {
-                return ctx;
-            }
-        }
-        //返回数据的副本
-        return cloneDeep(ctx);
-    };
+    });
 
 
     //新建默认数据中心
-        //理解这步很关键
-        //相当于在闭包中声明一个实例,
-        // 每次调用构造函数Data上面的静态方法相当于都是调用此实例中的原型上的方法,
-        //返回变量值, 也都是返回此实例中的_context属性中的数据;
     var data = new Data();
     console.log(data);
     //console.log(data._context)
     //console.log(data._events)
     //扩展Data接口
+    /*extendDeep(Data, {      //添加静态属性和方法
+     version: '0.2.1',
+     has: function (key) {
+     return data.has(key);
+     },
+     get: function (key) {
+     return data.get(key);
+     },
+     set: function (key, val) {
+     return data.set(key, val);
+     },
+     sub: function (type, key, callback) {
+     return data.sub(type, key, callback);
+     },
+     unsub: function (type, key, id) {
+     return data.unsub(type, key, id);
+     }
+     });*/
 
     //添加静态属性和方法
     Data.version = '0.2.1';
@@ -248,14 +307,12 @@
     Data.set = function (key, val) {
         return data.set(key, val);
     };
-    //return Data;
-    var _Data = root.Data;// Browser globals
-
-    Data.noConflict = function () {
-        if (root.Data === Data) {
-            root.Data = _Data;
-        }
-        return Data;
+    Data.sub = function (type, key, callback) {
+        return data.sub(type, key, callback);
     };
-    root.Data = Data;
-})(window);
+    Data.unsub = function (type, key, id) {
+        return data.unsub(type, key, id);
+    };
+    //console.log(data)
+    return Data;
+}));
