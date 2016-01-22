43ef474f8312cb4ff59ed6e1b4e51180cf7fe986
349057996@qq.com <ywy>
2016-01-22 17:33:41 +0800

    note of js's plugin

--- js/hd.js
+++ js/hd.js
@@ -18,6 +18,19 @@
      * Date.prototype.getTime()≈Date.prototype.valueOf()>+Date≈Number(Date)>>Date.parse()
      *
      *
+     //Fisher-Yates 随机算法
+     function shuffle(array) {
+        var copy = [],
+            n = array.length,
+            i;
+        // 如果还剩有元素。。
+        while (n) {
+            // 随机选取一个元素
+            i = Math.floor(Math.random() * n--);
+            copy.push(array.splice(i, 1)[0]);
+        }
+        return copy;
+     }
      */
 
     // "use strict";
