0f9301f5058c9566102f97b0a6b03a5bd37a1dd9
yinweiyi <349057996@qq.com>
2016-08-31 20:46:48 +0800

    note of js

--- practice/test2/js/my/fui.js
+++ practice/test2/js/my/fui.js
@@ -249,6 +249,23 @@
             return array.length > 0 ? array.concat([]) : array
         }
 
+        function type(obj) {
+            var toString = Object.prototype.toString;
+            var map = {
+                '[object Boolean]'  : 'boolean',
+                '[object Number]'   : 'number',
+                '[object String]'   : 'string',
+                '[object Function]' : 'function',
+                '[object Array]'    : 'array',
+                '[object Date]'     : 'date',
+                '[object RegExp]'   : 'regExp',
+                '[object Undefined]': 'undefined',
+                '[object Null]'     : 'null',
+                '[object Object]'   : 'object'
+            };
+            return map[toString.call(obj)];
+        }
+
         if (!Object.create) {
             Object.create = function (o) {
                 function F() {
